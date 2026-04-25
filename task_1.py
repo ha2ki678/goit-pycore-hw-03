a4d1f22d94d46e452ab869c6405a149b42045212
ha2ki678 <caskona228678@gmail.com>
2026-04-26 01:41:10 +0200

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,12 +1,16 @@
-import datetime
+from datetime import datetime
 
-def get_days_from_today(date_string):
+def get_days_from_today(date):
     try:
-        parsed_date = datetime.datetime.strptime(date_string, "%Y-%m-%d").date()
-    except:
-        return 0
+        given_date = datetime.strptime(date, "%Y-%m-%d").date()
+        today = datetime.today().date()
 
-    current_date = datetime.date.today()
+        difference = today - given_date
+        return difference.days
 
-    difference = current_date - parsed_date
-    return difference.days
+    except ValueError:
+        return "Неправильний формат дати. Використовуйте формат РРРР-ММ-ДД."
+
+user_date = input("Введіть дату у форматі РРРР-ММ-ДД: ")
+result = get_days_from_today(user_date)
+print(result)
